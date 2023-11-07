df3f17bc62bb50e72b6987bd619483cd2b7413b0
HP <HP@Ankur>
2023-11-07 13:05:26 +0530

    this is new commit msg.

---
+++ AdvanceAutomationFramework/src/test/java/AddToCart/TC_100.java
@@ -0,0 +1,18 @@
+package AddToCart;
+
+import org.testng.annotations.Test;
+
+import genericUtilities.BaseClass;
+
+public class TC_100 extends BaseClass
+
+{
+	@Test
+	public void demo()
+	{
+		System.out.println("hey");
+	}
+	
+	
+ 
+}
