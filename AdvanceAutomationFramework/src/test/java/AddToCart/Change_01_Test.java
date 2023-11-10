387d43379451f05d7aca2c07691bcac0ba7b6385
Saba Perween <Saba Perween@Saba>
2023-11-10 12:59:08 +0530

    This is 4th commit

---
+++ AdvanceAutomationFramework/src/test/java/AddToCart/Change_01_Test.java
@@ -0,0 +1,16 @@
+package AddToCart;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import genericUtilities.BaseClass;
+import genericUtilities.ItestListnerUtility;
+@Listeners(ItestListnerUtility.class)
+public class Change_01_Test extends BaseClass {
+@Test
+	public void change() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
